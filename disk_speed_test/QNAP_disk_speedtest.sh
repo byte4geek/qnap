cbe934db4595b22a55371b0147eb80dae8f02af5
byte4geek <admin@byte4geek.com>
2015-10-08 15:37:55 +0200

    Update QNAP_disk_speedtest.sh

--- disk_speed_test/QNAP_disk_speedtest.sh
+++ disk_speed_test/QNAP_disk_speedtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ###################################
 # speed disk test for QNAP 4 bays #
-#         witho log file          #
+#         with log file           #
 #         by PeppeBytes           #
 #        Oct-08-2015 v 1.0        #
 ###################################
